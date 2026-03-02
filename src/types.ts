fa1f29508e0ddbab586d6b4bb70b853b938f3837
Reuben <reuben.gorda@gmail.com>
2026-03-02 21:21:48 +0800

    add fasting window options

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,7 @@
-import { Schema } from '@livestore/livestore'
+import { Schema } from "@livestore/livestore";
 
-export const Filter = Schema.Literal('all', 'active', 'completed')
-export type Filter = typeof Filter.Type
+export const Filter = Schema.Literal("all", "active", "completed");
+export type Filter = typeof Filter.Type;
+
+export const FastingWindow = Schema.Literal(16, 18, 20, 24, 36, 48, 72);
+export type FastingWindow = typeof FastingWindow.Type;
