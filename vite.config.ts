3764d032286f2f82f95d274d1d6fbb5fc30f4572
Reuben <reuben.gorda@gmail.com>
2026-03-04 18:19:43 +0800

    rebuild gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,9 @@ export default defineConfig({
     react(),
     livestoreDevtoolsPlugin({ schemaPath: "./src/livestore/schema.ts" }),
   ],
+  build: {
+    outDir: "dist",
+    assetsDir: "",
+  },
+  base: "/fastrack/",
 });
