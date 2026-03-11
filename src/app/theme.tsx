c8b9083b2d2176580c8268028f59511c3cb1a249
Reuben Mark Gorda <reuben.gorda@gmail.com>
2026-03-11 15:19:12 +0800

    add custom palette for every color scheme

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -2,27 +2,46 @@ import { createTheme } from "@mui/material";
 
 export const appTheme = createTheme({
   colorSchemes: {
-    light: true,
-    dark: true,
-  },
-  palette: {
-    background: {
-      default: "#F2EFE7",
+    light: {
+      palette: {
+        background: {
+          default: "#F2EFE7",
+        },
+        primary: {
+          main: "#006A71ff",
+        },
+        secondary: {
+          main: "#48A6A7ff",
+        },
+        info: {
+          main: "#9ACBD0ff",
+        },
+        error: {
+          main: "#bc4749ff",
+        },
+        warning: {
+          main: "#f2e8cfff",
+        },
+      },
     },
-    primary: {
-      main: "#006A71ff",
-    },
-    secondary: {
-      main: "#48A6A7ff",
-    },
-    info: {
-      main: "#9ACBD0ff",
-    },
-    error: {
-      main: "#bc4749ff",
-    },
-    warning: {
-      main: "#f2e8cfff",
+    dark: {
+      palette: {
+        primary: {
+          main: "#006A71ff",
+        },
+        secondary: {
+          main: "#48A6A7ff",
+        },
+        info: {
+          main: "#9ACBD0ff",
+        },
+        error: {
+          main: "#bc4749ff",
+        },
+        warning: {
+          main: "#f2e8cfff",
+        },
+      },
     },
   },
 });
